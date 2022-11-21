1e57c473ba5cd660c772fe924775d354de55edd5
daffaalmerf <daffaalmerf@gmail.com>
2022-11-22 06:59:13 +0700

    add sqlalchemy and routes for konten, media, and comment

--- api_studia/sql_app/models.py
+++ api_studia/sql_app/models.py
@@ -29,7 +29,51 @@ class Tugas(Base):
     description = Column(Text)
     deadline = Column(String)
 
-    rel_kelas = relationship("Kelas", back_populates="rel_tugas")
+    rel_kelas_tugas = relationship("Kelas", back_populates="rel_tugas_kelas")
+
+
+class MediaPhoto(Base):
+    __tablename__ = "media_photo"
+
+    id = Column(Integer, primary_key=True, index=True)
+    konten_id = Column(String(10), ForeignKey("konten.id"))
+    name = Column(String(50))
+    url = Column(String(255))
+    base_url = Column(String(255))
+    size = Column(String(255))
+
+    rel_konten_media = relationship(
+        "Konten", back_populates="rel_media_konten")
+
+
+class Comment(Base):
+    __tablename__ = "comment"
+
+    id = Column(Integer, primary_key=True, index=True)
+    konten_id = Column(String(10))
+    user_id = Column(Integer)
+    release_date = Column(String)
+    text = Column(Text)
+
+    rel_konten_comment = relationship(
+        "Konten", back_populates="rel_comment_konten")
+
+
+class Konten(Base):
+    __tablename__ = "konten"
+
+    id = Column(String(10), primary_key=True, index=True)
+    kelas_id = Column(String(10), ForeignKey("kelas.id"))
+    photo_id = Column(String(10), ForeignKey("kelas.id"))
+    name = Column(String(255))
+    release_date = Column(String)
+    synopsis = Column(Text)
+
+    rel_kelas_konten = relationship("Kelas", back_populates="rel_konten_kelas")
+    rel_media_konten = relationship(
+        "MediaPhoto", back_populates="rel_konten_media")
+    rel_comment_konten = relationship(
+        "Comment", back_populates="rel_konten_comment")
 
 
 class Kelas(Base):
@@ -40,4 +84,6 @@ class Kelas(Base):
     section = Column(String(100))
     code = Column(String(100))
 
-    rel_tugas = relationship("Tugas", back_populates="rel_kelas")
+    rel_tugas_kelas = relationship("Tugas", back_populates="rel_kelas_tugas")
+    rel_konten_kelas = relationship(
+        "Konten", back_populates="rel_kelas_konten")
--- api_studia/sql_app/schemas.py
+++ api_studia/sql_app/schemas.py
@@ -20,6 +20,63 @@ class Tugas(TugasBase):
         orm_mode = True
 
 
+class MediaPhotoBase(BaseModel):
+    id: int
+    konten_id: str
+    name: str
+    url: str
+    base_url: str
+    size: str
+
+
+class MediaPhotoCreate(MediaPhotoBase):
+    pass
+
+
+class MediaPhoto(MediaPhotoBase):
+
+    class Config:
+        orm_mode = True
+
+
+class CommentBase(BaseModel):
+    id: int
+    konten_id: str
+    user_id: int
+    release_date: str
+    text: str
+
+
+class CommentCreate(CommentBase):
+    pass
+
+
+class Comment(CommentBase):
+
+    class Config:
+        orm_mode = True
+
+
+class KontenBase(BaseModel):
+    id: str
+    kelas_id: str
+    photo_id: str
+    name: str
+    release_date: str
+    synopsis: str
+
+
+class KontenCreate(KontenBase):
+    pass
+
+
+class Konten(KontenBase):
+    comment_list: list[Comment] = []
+
+    class Config:
+        orm_mode = True
+
+
 class KelasBase(BaseModel):
     id: str
     name: str
@@ -33,6 +90,7 @@ class KelasCreate(KelasBase):
 
 class Kelas(KelasBase):
     tugas_list: list[Tugas] = []
+    konten_list: list[Konten] = []
 
     class Config:
         orm_mode = True
